44fa73561401ddc392c98511870fe55119bd158a
ceorles <berches.avrles09@gmail.com>
2025-04-12 22:20:45 +0800

    Update App.js

--- musicplayer_react/src/App.js
+++ musicplayer_react/src/App.js
@@ -1,10 +1,12 @@
 import SpotifySearch from './components/search';
+import SpotifyRandomAlbum from './components/album';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <SpotifySearch />
+      <SpotifyRandomAlbum />
     </div>
   );
 }
